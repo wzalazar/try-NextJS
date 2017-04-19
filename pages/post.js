d4d62f2af26b7112696849c04603e73cb4ef52d2
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-19 15:09:28 -0300

    Initial commit

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,22 @@
 import Layout from '../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
 
-export default (props) => (
+const Post =  (props) => (
     <Layout>
-       <h1>{props.url.query.title}</h1>
-       <p>This is the blog post content.</p>
+       <h1>{props.movie.Title}</h1>
+       <p>{props.movie.Plot}</p>
+       <img src={props.movie.Poster}/>
     </Layout>
 )
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`http://www.omdbapi.com/?i=${id}`)
+  const movie = await res.json()
+
+  console.log(`Fetched movie: ${movie.Title}`)
+
+  return { movie }
+}
+
+export default Post
